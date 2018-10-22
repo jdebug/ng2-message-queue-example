2ca8062d31d8eb4b58d1432932ac5bb016e9ee3d
jdebug <Senthil_Jeganathan@cable.comcsat.com>
2018-10-22 05:54:49 -0400

    Updated to correct.

--- src/app/publisher/publisher.component.ts
+++ src/app/publisher/publisher.component.ts
@@ -2,12 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
 
-import {
-   debounceTime, distinctUntilChanged, switchMap
- } from 'rxjs/operators';
-
 import { Ng2MessageQueue } from 'ng2-message-queue';
-
 
 @Component({
   selector: 'app-publisher',
@@ -15,29 +10,18 @@ import { Ng2MessageQueue } from 'ng2-message-queue';
   styleUrls: [ './publisher.component.css' ]
 })
 export class PublisherComponent implements OnInit {
-  private searchTerms = new Subject<string>();
+  private searchTerms: string;
 
   constructor(private messageService: Ng2MessageQueue) {
     messageService.createQueue('myQueue');
   }
 
-  // Push a search term into the observable stream.
+  // Push a search term into the queue.
   search(term: string): void {
-    this.searchTerms.next(term);
-
     this.messageService.publish('myQueue', term);
   }
 
-  ngOnInit(): void {
-    // this.heroes$ = this.searchTerms.pipe(
-    //   // wait 300ms after each keystroke before considering the term
-    //   debounceTime(300),
-
-    //   // ignore new term if same as previous term
-    //   distinctUntilChanged(),
-
-    //   // switch to new search observable each time the term changes
-    //   // switchMap((term: string) => this.heroService.searchHeroes(term)),
-    // );
+  ngOnInit() {
+    
   }
 }
